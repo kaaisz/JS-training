8bb7f3455f3cbb45edec14a97dcc2af97df4e03c
Kaai Suzuki <suzydp@me.com>
2018-09-06 04:58:06 -0700

    03 - Activate slider

--- 08-Simple-Password-Generator/script.js
+++ 08-Simple-Password-Generator/script.js
@@ -1,1 +1,10 @@
-'use strict';
+(function(){
+    'use strict';
+    var slider = document.getElementById('slider');
+    var label = document.getElementById('label');
+
+    slider.addEventListener('change', function () {//when the number changed, this function starts
+        label.innerHTML = this.value;
+    });
+})();
+
