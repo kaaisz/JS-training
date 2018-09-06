4e24195749a48443a5784d53da98a3479de6e591
Kaai Suzuki <suzydp@me.com>
2018-09-06 06:16:52 -0700

    05 - Generate password randomly by using Math.floor and Math.random

--- 08-Simple-Password-Generator/script.js
+++ 08-Simple-Password-Generator/script.js
@@ -6,7 +6,22 @@
     var result = document.getElementById('result');
 
     function getPassword(){
-        result.value = 'asdfuikew';
+        var seed = 'abcdefghijklmnopqrstuvwxyz';//default setting
+        var length = slider.value;//how many numbers are required to password
+        var password = '';
+
+        //generate password randomly until numbers of slider.value ends
+        // for(var i = 0; i < length; i++){
+        //     password += seed[Math.floor(Math.random()*seed.length)];//to generate number randomly
+        // }
+
+        //another way - use while expression
+        while(length--){//until length ends
+            password += seed[Math.floor(Math.random() * seed.length)];
+        }
+
+        // result.value = 'asdfuikew';//dummy
+        result.value = password;
     }
 
     slider.addEventListener('change', function () {//when the number changed, this function starts
@@ -14,7 +29,7 @@
     });
 
     btn.addEventListener('click', function () {//when button has been clicked, this function starts
-        getPassword();//function
+        getPassword();
         // result.value = 'asdfuikew';//when button has been clicked, this value has been shown.
     });
 
@@ -22,6 +37,6 @@
         this.select();//at the same time with the result shows, result has been seleced.
     });
 
-    getPassword();
+    getPassword();//to get password from first loading 
 })();
 
