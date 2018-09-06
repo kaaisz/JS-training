e7bfb7a65708b4f0f07537deb1650f1753f04bff
Kaai Suzuki <suzydp@me.com>
2018-09-06 05:15:42 -0700

    04 - Activate button and password form

--- 08-Simple-Password-Generator/script.js
+++ 08-Simple-Password-Generator/script.js
@@ -2,9 +2,26 @@
     'use strict';
     var slider = document.getElementById('slider');
     var label = document.getElementById('label');
+    var btn = document.getElementById('btn');
+    var result = document.getElementById('result');
+
+    function getPassword(){
+        result.value = 'asdfuikew';
+    }
 
     slider.addEventListener('change', function () {//when the number changed, this function starts
         label.innerHTML = this.value;
     });
+
+    btn.addEventListener('click', function () {//when button has been clicked, this function starts
+        getPassword();//function
+        // result.value = 'asdfuikew';//when button has been clicked, this value has been shown.
+    });
+
+    result.addEventListener('click', function () {//when result input has been clicked
+        this.select();//at the same time with the result shows, result has been seleced.
+    });
+
+    getPassword();
 })();
 
