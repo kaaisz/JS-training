5586a6e6db03f8f2204d3fdef9a424f0b168767c
Kaai Suzuki <suzydp@me.com>
2018-09-06 00:59:04 -0700

    Rewrite - JS02 - Set the function to calculate

--- 03-Automatic-Calc/script.js
+++ 03-Automatic-Calc/script.js
@@ -4,19 +4,33 @@
   var price = document.getElementById('price');
   var num = document.getElementById('num');
   var unit = document.getElementById('unit');
-  var button = document.getElementById('button');
+  var btn = document.getElementById('button');
   var result = document.getElementById('result');
   var reset = document.getElementById('reset');
 
-  btn.addEventListener('click', function(){
+  btn.addEventListener('click', function() {
+    console.log('pushed btn');
+    var payLess;
+    var short;
+    var payMore;
+    var over;
+    var str;//to show result
+
     //case A(不足) : paid 300JPY/Person (payLess)- 100JPY short(short)
     // payLess = 1000 / 3; //333.333...
-    // payLess = 1000 / 3 / 100 //3.3333... 
-    payLess = Math.floor(1000 / 3 / 100) * 100; //300
-    short = 1000 - (300 * 3); //100
+    // payLess = 1000 / 3 / 100; //3.3333... 
+    payLess = Math.floor(price.value / num.value / unit.value) * unit.value; //300
+    short = price.value - (payLess * num.value); //100
 
     //case B(余り) : paid 400JPY/Person (payMore)- 200JPY over(over)
-    payMore = Math.ceil(1000 / 3 / 100) * 100; //400
-    over = 1000 - (400 * 3); //-200
+    payMore = Math.ceil(price.value / num.value / unit.value) * unit.value; //400
+    over = price.value - (payMore * num.value); //-200
+
+    //set results for each case
+    str = 'If the price was ¥' + payLess + ' / person, paybill will short ¥' + short + ', ' +
+      'If the price was ¥' + payMore + ' / person, paybill will over ¥' + over + '.';
+
+    //show the result
+    result.textContent = str;
   });
 })();
