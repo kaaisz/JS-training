d7e0207865beff2b83e7c25dccfc126ed732d69f
Kaai Suzuki <suzydp@me.com>
2018-09-08 13:21:55 -0700

    04 - Add JS to activate menu bar

--- 11-Tab-Menu-by-JS/script.js
+++ 11-Tab-Menu-by-JS/script.js
@@ -1,3 +1,24 @@
 (function(){
     'use strict';
+
+    var menuItems = document.getElementsByClassName('menu_item');
+    var i;
+
+    for(i = 0; i < menuItems.length; i++){//as long as there's a menu, do the action below
+        ///add event for each menu
+        menuItems[i].addEventListener('click', function(e){
+            //Set to disabled jumping to link address
+            e.preventDefault();//preventDefault = cancel event
+
+            //control active class
+            var i;//!!this is not the same with other 'i' above
+            for(i = 0; i < menuItems.length; i++){
+                ///once change to 'menu_item for all class when the event has activated
+                menuItems[i].className = 'menu_item';
+            }
+            //only <a> class which clicked, it'll be active.
+            //(because event has been set on only <a href="#" class="menu_item">...</a>)
+            this.className = 'menu_item active';
+        });
+    }
 })();
