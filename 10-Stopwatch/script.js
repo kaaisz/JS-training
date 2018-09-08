fd3672697079091035e9d52c212b6316a20b7884
Kaai Suzuki <suzydp@me.com>
2018-09-07 17:22:40 -0700

    04 - Fix the numbers of digits on timer

--- 10-Stopwatch/script.js
+++ 10-Stopwatch/script.js
@@ -6,7 +6,7 @@
     var reset = document.getElementById('reset');
 
     var startTime;
-    var elapsedTime = 0;//time passed
+    var elapsedTime = 0;//time passed. initialise as 0
 
     function updateTimerText(){
         //eg : 132.5 sec -> 135200msec -> 02:15.200 elapsed
@@ -16,6 +16,16 @@
         var m = Math.floor(elapsedTime / 60000);
         var sec = Math.floor(elapsedTime % 60000 / 1000);
         var msec = elapsedTime % 1000;
+
+        //to fix the number of digits
+        //eg : 3min or 12min -> '03' or '12' min
+        //JSには文字を連結させると文字列になるという性質がある
+        // 3 -> '0' + 3 => '03' / 12 -> '0' + 12 => '012'
+        //=> よって、0の文字列を連結し、そのあとに文字列の末尾2桁を"slice(-2)"で負の値を引数に渡すことで取得する
+        m = ('0' + m).slice(-2);
+        sec = ('0' + sec).slice(-2);
+        msec = ('00' + msec).slice(-3);//because of only msec has 3 digits
+
         //show the output on HTML
         timer.textContent = m + ':' + sec + '.' + msec;
     }
