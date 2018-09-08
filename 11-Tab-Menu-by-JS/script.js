f4215e7f37beb49b26fcebf8028aad069bed28d4
Kaai Suzuki <suzydp@me.com>
2018-09-08 13:45:38 -0700

    05 - Activate contents of tab menu - combine tab and content

--- 11-Tab-Menu-by-JS/script.js
+++ 11-Tab-Menu-by-JS/script.js
@@ -2,6 +2,7 @@
     'use strict';
 
     var menuItems = document.getElementsByClassName('menu_item');
+    var contents = document.getElementsByClassName('content');
     var i;
 
     for(i = 0; i < menuItems.length; i++){//as long as there's a menu, do the action below
@@ -19,6 +20,13 @@
             //only <a> class which clicked, it'll be active.
             //(because event has been set on only <a href="#" class="menu_item">...</a>)
             this.className = 'menu_item active';
+
+            for(i = 0; i < contents.length; i++){
+                contents[i].className = 'content';
+            }
+
+            document.getElementById(this.dataset.id).className = 'content active';
+            //details of 'data-id' can pick up by "this.dataset.id".
         });
     }
 })();
