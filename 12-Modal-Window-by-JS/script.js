eed5688272330ddd035de5a5186deb26207456c1
Kaai Suzuki <suzydp@me.com>
2018-09-08 20:21:09 -0700

    【12-Modal-Window-by-JS】06 - Add click event

--- 12-Modal-Window-by-JS/script.js
+++ 12-Modal-Window-by-JS/script.js
@@ -1,3 +1,27 @@
 (function(){
     'use strict';
+
+    //get element which added id
+    var open = document.getElementById('open');
+    var close = document.getElementById('close');
+    var modal = document.getElementById('modal');
+    var mask = document.getElementById('mask');
+
+    //set click event
+    //when open button has been clicked
+    open.addEventListener('click', function(){
+        //add each className to blank (= removeclass)
+        modal.className = '';
+        mask.className = '';
+    });
+
+    //when close button has been clicked
+    close.addEventListener('click', function(){
+        //add each className to hidden
+        modal.className = 'hidden';
+        mask.className = 'hidden';
+        //same things happen below
+        // close.click();
+    });
+
 })();
