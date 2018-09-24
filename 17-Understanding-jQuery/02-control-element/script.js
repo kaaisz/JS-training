dcc8f101a2de03be9b10ee5b28422e68bc9c1e96
Kaai Suzuki <suzydp@me.com>
2018-09-23 19:48:31 -0700

    【17-Understanding-jQuery】05 - Understand how to change attribute to get class name on jQuery

--- 17-Understanding-jQuery/02-control-element/script.js
+++ 17-Understanding-jQuery/02-control-element/script.js
@@ -1,8 +1,12 @@
 (function($){
     //when entire page has loaded, excuse function
     $(document).ready(function(){
-        //if you'd like to use get id name of specified element, you can use "attr("id") behind element."
-        var id_name = $("p").attr("id");
-        $("#result").html(id_name);
+        //if you'd like to use get id name of specified element, you can use "attr("id") behind element.
+        // var id_name = $("p").attr("id");
+        
+        //if you'd like to change the attr name, rewrite the code like this below
+        var id_name = $("p:first").attr("id", "bar");
+
+        $("#result").html(id_name.attr("id"));
     });
 })(jQuery);
