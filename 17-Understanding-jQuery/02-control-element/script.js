720a0867328a82bc35ea59932856763809f2ea0c
Kaai Suzuki <suzydp@me.com>
2018-09-23 21:43:23 -0700

    【17-Understanding-jQuery】07 - Understand how to count numbers of element by using jQuery

--- 17-Understanding-jQuery/02-control-element/script.js
+++ 17-Understanding-jQuery/02-control-element/script.js
@@ -1,6 +1,7 @@
 (function($){
     //when entire page has loaded, excuse function
     $(document).ready(function(){
+        //Pt.1
         //if you'd like to use get id name of specified element, you can use "attr("id") behind element.
         // var id_name = $("p").attr("id");
         
@@ -11,9 +12,16 @@
     });
 
     $(document).ready(function(){
+        //Pt.2
         //get element of first <div>
         var contents = $("div:first").html();
         //if you'd like to change the content of element, write like this below
         $("div:eq(1)").html(contents);
+
+        //Pt.3
+        //If you'd like to count the number of elements
+        // you can count quantity of elements by using .length behind element
+        var num = $(".count").length;
+        $("#result2").html(num);
     });
 })(jQuery);
