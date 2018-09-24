2b77e4c49687780494bc903dc0c7f06b8b7b2fa1
Kaai Suzuki <suzydp@me.com>
2018-09-23 18:55:04 -0700

    【17-Understanding-jQuery】01 - Add introduction file

---
+++ 17-Understanding-jQuery/01-introduction/script.js
@@ -0,0 +1,19 @@
+/*
+jQuery has some of way to declare itself
+1 - 
+    jQuery(document).ready(function(){
+        //write code here
+    });
+
+2 -
+    $(function(){
+        //write code here
+    });
+
+3 - 
+    (see the code below)
+*/
+
+(function($){
+    //write code here
+})(jQuery);
