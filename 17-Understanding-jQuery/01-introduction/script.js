514ba3aa1049c4b4d6d3286c77a91f5a472092ab
Kaai Suzuki <suzydp@me.com>
2018-09-23 19:34:34 -0700

    【17-Understanding-jQuery】05 - Understand how to choose child class of tag name by using jQuery

--- 17-Understanding-jQuery/01-introduction/script.js
+++ 17-Understanding-jQuery/01-introduction/script.js
@@ -34,13 +34,23 @@ jQuery has some of way to declare itself
         //as you did last paragraph, you can also choose class like using "getElementsByClassName"
         var elem = $(".elems");
         elem.each(function(){
-            $(this).css("backgroundColor", "#0bf")
-        })
+            $(this).css("backgroundColor", "#0bf");
+        });
 
         //also you can choose tag like using "getElementsByTagName"
         var elem2 = $("span");
         elem2.each(function(){
-            $(this).css("backgroundColor", "#F80")
+            $(this).css("backgroundColor", "#F80");
         });
+
+        //So, if you want to choose first, last or some other numbers of child class of the tag, do like this
+        var elem3 = $("li:first");//equals li:first-child
+        elem3.css("backgroundColor", "#f0f");
+
+        var elem4 = $("li:eq(1)");//equals li:nth-child(2) *numbers of eq starts from "0(0, 1, 2)".
+        elem4.css("backgroundColor", "#e8e");
+
+        var elem5 = $("li:last");//equals li:last-child
+        elem5.css("backgroundColor", "#b5d");
     });
 })(jQuery);
