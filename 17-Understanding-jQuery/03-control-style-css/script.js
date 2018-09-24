50abf2e400599ea4af8f30893f781669a4e63aef
Kaai Suzuki <suzydp@me.com>
2018-09-24 04:00:36 -0700

    【17-Understanding jQuery】11 - Understanding how to change height of element

--- 17-Understanding-jQuery/03-control-style-css/script.js
+++ 17-Understanding-jQuery/03-control-style-css/script.js
@@ -1,7 +1,11 @@
 (function($){
     //when entire page has loaded, excuse function
     $(document).ready(function(){
+        //Pt.1 - Understanding how to show the style of element
         var result = $("#element1").height();//define variable result as "height of #element"
         $("#result1").html(result);
+
+        //Pt.2 - Understanding how to change the style of element
+        $("#element2").height("100px");
     });
 })(jQuery);
