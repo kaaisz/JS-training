814bdc6851e7264af3c83ab6b45323c4831ae031
Kaai Suzuki <suzydp@me.com>
2018-09-23 22:35:21 -0700

    【17-Understanding-jQuery】08 - Understanding how to replace any words in specified elements

--- 17-Understanding-jQuery/02-control-element/script.js
+++ 17-Understanding-jQuery/02-control-element/script.js
@@ -7,11 +7,8 @@
         
         //if you'd like to change the attr name, rewrite the code like this below
         var id_name = $("p:first").attr("id", "bar");
-
         $("#result").html(id_name.attr("id"));
-    });
 
-    $(document).ready(function(){
         //Pt.2
         //get element of first <div>
         var contents = $("div:first").html();
@@ -23,5 +20,12 @@
         // you can count quantity of elements by using .length behind element
         var num = $(".count").length;
         $("#result2").html(num);
+
+        //Pt.4
+        //If you'd like to replace the word in any specified elements, you can write code like below
+        $(".replace").each(function(){
+            $(this).html("Hello World");
+            //only ".replace" will change from "This is p element" to "Hello World"
+        });
     });
 })(jQuery);
