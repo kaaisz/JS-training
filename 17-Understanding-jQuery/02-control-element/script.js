5e6b99b26134baea40ce87ec06b4e5c5f58ff5cc
Kaai Suzuki <suzydp@me.com>
2018-09-23 19:59:07 -0700

    【17-Understanding-jQuery】06 - Understand how to change inside of element by using jQuery

--- 17-Understanding-jQuery/02-control-element/script.js
+++ 17-Understanding-jQuery/02-control-element/script.js
@@ -9,4 +9,11 @@
 
         $("#result").html(id_name.attr("id"));
     });
+
+    $(document).ready(function(){
+        //get element of first <div>
+        var contents = $("div:first").html();
+        //if you'd like to change the content of element, write like this below
+        $("div:eq(1)").html(contents);
+    });
 })(jQuery);
