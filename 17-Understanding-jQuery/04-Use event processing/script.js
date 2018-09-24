83910d67a7216271916fc92a5031bed983a1a55e
Kaai Suzuki <suzydp@me.com>
2018-09-24 06:24:24 -0700

    【17-Understanding-jQuery】18 - Understanding event when it hovered

--- 17-Understanding-jQuery/04-Use event processing/script.js
+++ 17-Understanding-jQuery/04-Use event processing/script.js
@@ -2,9 +2,15 @@
     //when entire page has loaded, excuse function
     $(document).ready(function(){
         //Pt.1 - Start processing when it has been clicked
-        $("#elem").click(function(){//processing starts when #elem has been clicked
+        $("#elem-click").click(function(){//processing starts when #elem has been clicked
             //count numbers of time of it clicked
-            $("#result").html(parseInt($("#result").html())+1);
+            $("#result-click").html(parseInt($("#result-click").html())+1);
+        });
+
+        //Pt.2 - Start processing when mouse has hovered on the element
+        $("#elem-hover").hover(function(){
+            //count numbers of time of it hovered
+            $("#result-hover").html(parseInt($("#result-hover").html()) + 1);
         });
     });
 })(jQuery);
