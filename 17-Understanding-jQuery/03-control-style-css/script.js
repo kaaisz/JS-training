56ddb4c3f7a25cda990f3c71333be495a8dcc80c
Kaai Suzuki <suzydp@me.com>
2018-09-24 04:17:32 -0700

    【17-Understanding jQuery】13 - Understanding how to change width of element

--- 17-Understanding-jQuery/03-control-style-css/script.js
+++ 17-Understanding-jQuery/03-control-style-css/script.js
@@ -11,5 +11,8 @@
         //Pt.3 - Understanding how to Get width of element
         var result3 = $("#element3").width();//define variable result as "height of #element"
         $("#result3").html(result3);
+
+        //Pt.4 - Understanding how to change the style of element
+        $("#element5").width("100px");
     });
 })(jQuery);
