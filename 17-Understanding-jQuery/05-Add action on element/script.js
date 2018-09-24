777bcfd2265e01596287fda99d8e132d78a1468d
Kaai Suzuki <suzydp@me.com>
2018-09-24 10:14:47 -0700

    【17-Understanding-jQuery】23 - Understanding how show() works on jQuery

--- 17-Understanding-jQuery/05-Add action on element/script.js
+++ 17-Understanding-jQuery/05-Add action on element/script.js
@@ -4,7 +4,13 @@
         //Pt.1 - Hide specified elements
         $("#btn-hide").click(function(){//processing starts when #btn-hide has been clicked
             //add action for when it has been clicked
-            $("#elem-hide").hide();
+            $(".elem-hide").hide();
+        });
+
+        //Pt.2 - Show specified elements
+        $("#btn-show").click(function () {//processing starts when #btn-show has been clicked
+            //add action for when it has been clicked(when it has disappeared)
+            $(".elem-hide").show();
         });
     });
 })(jQuery);
