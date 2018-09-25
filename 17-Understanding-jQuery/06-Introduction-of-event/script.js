1644d791170998d602de7defa346398d0d75150d
Kaai Suzuki <suzydp@me.com>
2018-09-25 00:30:28 -0700

    【17-Understanding-jQuery】3 - Understanding how to declare jQuery event

--- 17-Understanding-jQuery/06-Introduction-of-event/script.js
+++ 17-Understanding-jQuery/06-Introduction-of-event/script.js
@@ -1,4 +1,5 @@
 (function($){
+    //Pt.1 - understanding fundamental of event
     $(function(){
         /*
         the event of jQuery should write code as below
@@ -6,9 +7,30 @@
         
         - Event name must specify event name what you want to use by string type.
         - Event handler must have function you want to execute when the event has been activated.
+
+        also, you can use like this when add ready event
+            $(function () { ... });
+            $(document).ready(function () { ... });
+            $().ready(function () { ... });
         */
        $("#btn").on('click', function(){
             alert("Event has happened!");
        });
+
+    });
+
+    //Pt.2 - Understanding how to declare event
+    function logEvent(log){
+        $("#log1").append($("<li />").text(log + "has happened."));
+    }
+    
+    $(function(){
+        logEvent("$(function(){...})");
+    });
+    $(document).ready(function(){
+        logEvent("$(document).ready(function(){...})");
+    });
+    $().ready(function(){
+        logEvent('$().ready(function(){...})');
     });
 })(jQuery);
