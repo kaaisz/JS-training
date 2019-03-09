1376e54a15a4a6eabaf50b192dbeb4312513cea7
Kaai Suzuki <suzydp@me.com>
2019-03-09 13:50:38 -0800

    【24-page-transition-with-barba-js】Replace codes to Vanilla.js

--- 24-page-transition-with-barba-js/main.js
+++ 24-page-transition-with-barba-js/main.js
@@ -16,39 +16,14 @@ var FadeTransition = Barba.BaseTransition.extend({
       .then(this.fadeIn.bind(this));
   },
 
-  fadeOut: function() {
-    /**
-     * this.oldContainer is the HTMLElement of the old Container
-     */
+  // new container - current page after jumped
+  // old container - previous page before jump to the next page
 
-    return $(this.oldContainer).animate({ opacity: 0 }).promise();
+  fadeOut: function() {
   },
 
   fadeIn: function() {
-    /**
-     * this.newContainer is the HTMLElement of the new Container
-     * At this stage newContainer is on the DOM (inside our #barba-container and with visibility: hidden)
-     * Please note, newContainer is available just after newContainerLoading is resolved!
-     */
-
-    var _this = this;
-    var $el = $(this.newContainer);
-
-    $(this.oldContainer).hide();
-
-    $el.css({
-      visibility : 'visible',
-      opacity : 0
-    });
-
-    $el.animate({ opacity: 1 }, 400, function() {
-      /**
-       * Do not forget to call .done() as soon your transition is finished!
-       * .done() will automatically remove from the DOM the old Container
-       */
-
-      _this.done();
-    });
+    this.newContainer.classList.add("slide-in");
   }
 });
 
