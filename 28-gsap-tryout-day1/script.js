86dbf3be0c6e798eae4437567001183310a0d3d4
Kaai Suzuki <suzydp@me.com>
2020-06-27 14:55:54 -0700

    【28-gsap-tryout-day1】set navbar animation

--- 28-gsap-tryout-day1/script.js
+++ 28-gsap-tryout-day1/script.js
@@ -7,7 +7,7 @@
     });
  */
 
-// backgrond delay
+// background delay
 TweenMax.to (".bg", 2, {
     width: "100%",
     ease: Expo.easeInOut
@@ -21,6 +21,31 @@ TweenMax.to (".bg", 2, {
         ease: Expo.easeInOut - https://greensock.com/docs/v2/Easing/Expo
     });
  */
+
+// navbar
+TweenMax.from (".navbar", 1.5, {
+    delay: 2.5,
+    opacity: 0,
+    y: 20,
+    ease: Expo.easeInOut
+});
+
+/**
+ *  TweenMax.staggerFrom (element, duration as seconds, {
+ *      - https://greensock.com/docs/v2/TimelineMax/staggerFrom() -
+        delay: 2.8,
+        y: "100%",
+        ease: Expo.easeInOut - https://greensock.com/docs/v2/Easing/Expo
+    }, 0.08); - animation appear each
+ */
+
+// social media
+TweenMax.staggerFrom (".media ul li", 1.5, {
+    delay: 2.8,
+    opacity: 0,
+    x: -30,
+    ease: Expo.easeInOut
+}, 0.08);
 
 // text delay
 TweenMax.from (".text h1 .hide", 1.5, {
