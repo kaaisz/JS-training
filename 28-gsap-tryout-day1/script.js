ee75a48a9c8f9a882947b294b5404c466fbe7503
Kaai Suzuki <suzydp@me.com>
2020-06-27 13:15:31 -0700

    【28-gsap-tryout-day1】Apply text tween

---
+++ 28-gsap-tryout-day1/script.js
@@ -0,0 +1,22 @@
+/**
+ *  TweenMax.from (element, duration as seconds, {
+ *      -Tweenmax properties https://greensock.com/docs/v2/TweenMax -
+        delay: 1.6,
+        y: "100%",
+        ease: Expo.easeInOut - https://greensock.com/docs/v2/Easing/Expo
+    });
+ */
+
+
+// text delay
+TweenMax.from (".text h1 .hide", 1.5, {
+    delay: 1.6,
+    y: "100%",
+    ease: Expo.easeInOut
+});
+
+TweenMax.from (".text p .hide", 1.5, {
+    delay: 2,
+    y: "100%",
+    ease: Expo.easeInOut
+});
