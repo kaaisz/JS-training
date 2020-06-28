94f75c9f746e639973959f4a73ed4c1c581a8e17
Kaai Suzuki <suzydp@me.com>
2020-06-27 22:07:24 -0700

    【29-gsap-tryout-day2】Slider tween in progress

--- 29-gsap-tryout-day2/main.js
+++ 29-gsap-tryout-day2/main.js
@@ -1,6 +1,35 @@
-const swiper = new Swiper('.swiper-container', {
+const slider = new Swiper('.swiper-container', {
     slidesPerView: 'auto',
     spaceBetween: 150,
     centeredSlides: true,
     mousewheel: true
 })
+
+slider.on('slideChange', function() {
+    TweenMax.to('.slide-text span', .2, {
+        y: '-100px'
+    })
+    TweenMax.to('.slide-number span', .2, {
+        x: '-100px'
+    })
+    TweenMax.to('.swiper-slide-active', .5, {
+        scale: .85
+    })
+})
+
+// slider.on('slideChangeTransitionEnd', function() {
+//     TweenMax.to('.slide-text span', .2, {
+//         y: 0,
+//         delay: .5
+//     })
+//     TweenMax.to('.slide-text span', 0, {
+//         y: '100px',
+//     })
+//     TweenMax.to('.slide-number span', .2, {
+//         x: 0,
+//         delay: .7
+//     })
+//     TweenMax.to('.slide-number span', 0, {
+//         x: '100px',
+//     })
+// }
