19bc01164df4b43b7f1b8e3d53b3938114cdc21f
Kaai Suzuki <suzydp@me.com>
2020-06-28 18:16:52 -0700

    【30-gsap-tryout-day3】Add GSAP animation around bottom nav

--- 30-gsap-tryout-day3/script.js
+++ 30-gsap-tryout-day3/script.js
@@ -19,4 +19,20 @@
         x: 1000,
         ease: Circ.easeInOut
     })
+    // animate each bottom menu with foreach loop
+    TweenMax.staggerFrom ('.bottomnav ul li', 2, {
+        delay: 1,
+        x: 1000,
+        ease: Circ.easeInOut
+    }, 0.08)
+    TweenMax.from ('.info', 2, {
+        delay: 1.5,
+        y: 100,
+        ease: Circ.easeInOut
+    })
+    TweenMax.from ('.name', 2, {
+        delay: 1.5,
+        x: -600,
+        ease: Circ.easeInOut
+    })
 })();
