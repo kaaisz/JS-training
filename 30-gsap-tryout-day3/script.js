e844decdfce31241eadfee7cebb9c65d7e8206ff
Kaai Suzuki <suzydp@me.com>
2020-06-28 18:10:04 -0700

    【30-gsap-tryout-day3】Set GSAP animation around title

--- 30-gsap-tryout-day3/script.js
+++ 30-gsap-tryout-day3/script.js
@@ -9,4 +9,14 @@
         width: '40%',
         ease: Power2.easeInOut
     })
+    TweenMax.from ('.text h1', 2, {
+        delay: .6,
+        x: 1000,
+        ease: Circ.easeInOut
+    })
+    TweenMax.from ('.text h2', 2, {
+        delay: .7,
+        x: 1000,
+        ease: Circ.easeInOut
+    })
 })();
