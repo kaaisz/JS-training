b8d3070996dd01e877932f7ea15c6fd187d9017d
Kaai Suzuki <suzydp@me.com>
2020-06-28 19:00:58 -0700

    【30-gsap-tryout-day3】Mobile optimization

--- 30-gsap-tryout-day3/script.js
+++ 30-gsap-tryout-day3/script.js
@@ -1,14 +1,4 @@
 (function() {
-    TweenMax.to ('.left', 2, {
-        delay: .8,
-        width: '60%',
-        ease: Power2.easeInOut
-    })
-    TweenMax.to ('.right', 2, {
-        delay: .6,
-        width: '40%',
-        ease: Power2.easeInOut
-    })
     TweenMax.from ('.text h1', 2, {
         delay: .6,
         x: 1000,
@@ -18,11 +8,6 @@
         delay: .7,
         x: 1000,
         ease: Circ.easeInOut
-    })
-    TweenMax.to ('.anna', 2, {
-        delay: 1.5,
-        width: '800px',
-        ease: Power2.easeInOut
     })
     // animate each bottom menu with foreach loop
     TweenMax.staggerFrom ('.bottomnav ul li', 2, {
@@ -40,4 +25,40 @@
         x: -600,
         ease: Circ.easeInOut
     })
+
+    const mq = window.matchMedia('max-width: 767px');
+
+    if(mq.matches) {
+        TweenMax.to ('.left', 2, {
+            delay: .8,
+            width: '60%',
+            ease: Power2.easeInOut
+        })
+        TweenMax.to ('.right', 2, {
+            delay: .6,
+            width: '40%',
+            ease: Power2.easeInOut
+        })
+        TweenMax.to ('.anna', 2, {
+            delay: 1.5,
+            width: '800px',
+            ease: Power2.easeInOut
+        })
+    } else {
+        TweenMax.to ('.left', 2, {
+            delay: .8,
+            width: '80%',
+            ease: Power2.easeInOut
+        })
+        TweenMax.to ('.right', 2, {
+            delay: .6,
+            width: '20%',
+            ease: Power2.easeInOut
+        })
+        TweenMax.to ('.anna', 2, {
+            delay: 1.5,
+            width: '100vw',
+            ease: Power2.easeInOut
+        })
+    }
 })();
