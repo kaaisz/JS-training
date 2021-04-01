3553215561cbd548208332516b6f30d17864681b
Kaai Suzuki <suzydp@me.com>
2021-03-31 21:54:49 -0700

    【33-learn-react-cycle】Check if prevState in parameter works in setState function

--- 33-learn-react-cycle/src/App.js
+++ 33-learn-react-cycle/src/App.js
@@ -12,7 +12,9 @@ class App extends React.Component {
 
   handleClick = () => {
     // this is asyncronous setState
-    this.setState({meaningOfLife: this.state.meaningOfLife + 1}, 
+    this.setState((prevState, prevProps) => {
+      return {meaningOfLife: prevState.meaningOfLife + 1}
+    }, 
       // callback - this makes console log below sync
       () => console.log(this.state.meaningOfLife)
     )
